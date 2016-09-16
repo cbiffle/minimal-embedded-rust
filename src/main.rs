92d0b1ea2e2c82ef25c1e81d6328e985b6b2e0e2
Cliff L. Biffle <code@cliffle.com>
2016-09-16 08:31:55 -0700

    main: remove eh_personality.
    This isn't required when we set panic=abort.

--- src/main.rs
+++ src/main.rs
@@ -75,9 +75,6 @@ pub static ISR_VECTORS : ExceptionTable = ExceptionTable {
     sys_tick: Some(trap),
 };
 
-#[lang = "eh_personality"]
-extern fn eh_personality() {}
-
 #[lang = "panic_fmt"]
 extern fn panic_fmt() -> ! {loop {}}
 

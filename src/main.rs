4207960e0f9cb1464e1d3fc34668d417b1131a8c
Cliff L. Biffle <code@cliffle.com>
2016-09-15 19:35:59 -0700

    Sketch of a vector table.
    This doesn't work, because I can't correctly stuff the start vector in
    there.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,45 @@
 #![feature(lang_items)]
 #![feature(start)]
 
+extern {
+    /// This symbol is exported by the linker script, and defines the initial
+    /// stack pointer.  Because we put it into the vector table --- which we
+    /// model as an array of fn pointers --- we pun it as a function symbol
+    /// here.  This is kind of a hack, but a reasonable one that I've done in
+    /// C/C++ before too.
+    fn __STACK_BASE();
+
+    // TODO: model the ISRs as a proper struct and quit doing this.
+}
+
 #[start]
+#[inline(never)]  // for inspection
 pub fn start(_argc: isize, _argv: *const *const u8) -> isize {
     loop {}
 }
+
+unsafe extern "C" fn trap() { loop {} }
+
+#[link_section=".isr_vector"]
+pub static ISR_VECTORS: [Option<unsafe extern fn()>; 16] = [
+    Some(__STACK_BASE),
+    Some(trap),
+
+    Some(trap),             // NMI
+    Some(trap),             // Hard Fault
+    Some(trap),             // MemMang
+    Some(trap),             // BusFault
+    Some(trap),             // UsageFault
+    None,
+    None,
+    None,
+    None,
+    Some(trap),             // sv_call
+    Some(trap),             // DebugMon
+    None,
+    Some(trap),             // PendSV
+    Some(trap),             // SysTick
+];
 
 #[lang = "eh_personality"]
 extern fn eh_personality() {}
